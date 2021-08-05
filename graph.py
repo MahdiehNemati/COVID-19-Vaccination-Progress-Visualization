a000608d2fe593b2e4672c35bc68d618a9d65d3c
mohammad <mohammad_hasani@live.com>
2021-08-05 18:41:07 +0430

    create dynamic data as country

--- graph.py
+++ graph.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+from functions.get_data_by_iso import get_data_by_iso
+
+country_vaccinations_data = pd.read_csv('data/country_vaccinations.csv')
+
+aus_data = get_data_by_iso(country_vaccinations_data, 'AUS')
+
