a44268de15d231e99de620d5e900bd7d99f79b7e
MahdiehNemati <83155382+MahdiehNemati@users.noreply.github.com>
2021-08-06 20:17:50 +0930

    test

--- graph.py
+++ graph.py
@@ -1,5 +1,8 @@
-# First Commit
-
 import pandas as pd
 
-country_vaccination = pd.read_csv('data/country_vaccinations.csv')
+from functions.get_data_by_iso import get_data_by_iso
+
+country_vaccinations_data = pd.read_csv('data/country_vaccinations.csv')
+
+aus_data = get_data_by_iso(country_vaccinations_data, 'AUS')
+
