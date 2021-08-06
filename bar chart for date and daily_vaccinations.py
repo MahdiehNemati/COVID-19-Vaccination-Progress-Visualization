96e75cfc6e7f1462faa93d813797ba90250c6691
MahdiehNemati <83155382+MahdiehNemati@users.noreply.github.com>
2021-08-06 20:34:41 +0930

    bar chart for data and daily_vaccinations added

---
+++ bar chart for date and daily_vaccinations.py
@@ -0,0 +1,27 @@
+import inline as inline
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+from graph import country_vaccinations_data
+%matplotlib inline
+
+country_vaccinations_data.set_index('date',inplace=True)
+
+#set ggplot style
+plt.style.use('ggplot')
+
+#plot data
+fig, ax = plt.subplots(figsize=(15,7))
+ax.bar(country_vaccinations_data.index, country_vaccinations_data['daily_vaccinations'])
+
+#set ticks every week
+ax.xaxis.set_major_locator(mdates.MonthLocator())
+#format date
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+
+ax.set_title('')
+ax.set_ylabel('Count')
+ax.set_xlabel('Date')
+
+plt.show()
