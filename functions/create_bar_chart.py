9d10b98a952ea3848624f702a02cc010e3261021
MahdiehNemati <83155382+MahdiehNemati@users.noreply.github.com>
2021-08-06 22:58:29 +0930

    clean code and make function available for all countries

---
+++ functions/create_bar_chart.py
@@ -0,0 +1,25 @@
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def bar_chart(dataframe, title, ylabel, xlabel):
+    dataframe.loc[0:, "date"] = pd.to_datetime(dataframe['date'])
+    dataframe.set_index('date', inplace=True)
+
+    # set ggplot style
+    plt.style.use('ggplot')
+
+    # plot data
+    fig, ax = plt.subplots(figsize=(15, 7))
+    ax.bar(dataframe.index, dataframe['daily_vaccinations'])
+    # set ticks every week
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    # format date
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+
+    return plt
--- graph.py
+++ graph.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
+from functions.create_bar_chart import bar_chart
 from functions.get_data_by_iso import get_data_by_iso
 
 country_vaccinations_data = pd.read_csv('data/country_vaccinations.csv')
 
-aus_data = get_data_by_iso(country_vaccinations_data, 'AUS')
+country_data = get_data_by_iso(country_vaccinations_data, 'AFG')
 
+country_data_chart_title = ('Daily Vaccinations Progress of the {country_name} in 2021').format(
+    country_name=country_data.iloc[0]['country'])
+
+country_data_chart = bar_chart(dataframe=country_data, title=country_data_chart_title, ylabel='Daily Vaccinations',
+                               xlabel="Date")
+
+country_data_chart.show()
