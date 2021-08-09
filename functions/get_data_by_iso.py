1422e3e08f337b688a6ea8311612865b0e02601d
MahdiehNemati <83155382+MahdiehNemati@users.noreply.github.com>
2021-08-09 23:10:59 +0930

    pie chart added

--- functions/get_data_by_iso.py
+++ functions/get_data_by_iso.py
@@ -1,4 +1,14 @@
-def get_data_by_iso(df, iso_name: str):
-    csv_data = df.loc[df['iso_code'] == iso_name]
+def get_data_by_iso(dataframe, iso_name: str):
+    csv_data = dataframe.loc[dataframe['iso_code'] == iso_name]
+
+    return csv_data
+
+
+def get_distinct_vaccines_company(dataframe):
+    return dataframe['vaccine'].unique()
+
+
+def get_data_by_vaccine_name(dataframe, vaccine_name: str):
+    csv_data = dataframe.loc[dataframe['vaccine'] == vaccine_name]
 
     return csv_data
---
+++ run_stack_bar_chart_vaccination_progress_by_country.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from functions.create_stack_bar_chart import stack_bar_chart
+from functions.get_data_by_iso import get_data_by_iso
+
+country_vaccinations_data = pd.read_csv('data/country_vaccinations.csv')
+
+country_data = get_data_by_iso(country_vaccinations_data, 'IRN')
+
+country_data_chart_title = ('Comparison of first doze and full doze in {country_name} - 2021').format(
+    country_name=country_data.iloc[0]['country'])
+
+country_data_stack_bar_chart = stack_bar_chart(dataframe=country_data, title=country_data_chart_title,
+                                               ylabel='Vaccinations', xlabel='Date')
+
+country_data_stack_bar_chart.show()
