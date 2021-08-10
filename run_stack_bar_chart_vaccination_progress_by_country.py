49b93294e46b20ffd085d53170b548707a9c2bc1
MahdiehNemati <83155382+MahdiehNemati@users.noreply.github.com>
2021-08-10 18:03:03 +0930

    some changes in graph.py

--- run_stack_bar_chart_vaccination_progress_by_country.py
+++ run_stack_bar_chart_vaccination_progress_by_country.py
@@ -5,7 +5,7 @@ from functions.get_data_by_iso import get_data_by_iso
 
 country_vaccinations_data = pd.read_csv('data/country_vaccinations.csv')
 
-country_data = get_data_by_iso(country_vaccinations_data, 'IRN')
+country_data = get_data_by_iso(country_vaccinations_data, 'AUS')
 
 country_data_chart_title = ('Comparison of first doze and full doze in {country_name} - 2021').format(
     country_name=country_data.iloc[0]['country'])
